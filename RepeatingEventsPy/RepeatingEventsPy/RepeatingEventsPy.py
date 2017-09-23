760a8bdbe687e9f9ba72d93e6b02990dad8a35d2
Buddhi Adhikari <buddhiadikari@live.com>
2017-09-23 17:20:34 +0100

    Add files via upload

---
+++ RepeatingEventsPy/RepeatingEventsPy/RepeatingEventsPy.py
@@ -0,0 +1,50 @@
+import turtle
+
+#for count in range(4) :
+#    turtle.forward(100)
+#    turtle.right(90)
+#turtle.color('red')
+#turtle.forward(200)
+
+#for step in range(5):
+#    turtle.forward(100)
+#    turtle.right(360/5)
+#    for moreSteps in range(5):
+#        turtle.forward(50)
+#        turtle.right(360/5)
+
+
+
+
+##using count in for loops
+#for count in range(4):
+#    print(count)
+
+##for loop specifying the starting point
+#for count in range(1,4):
+#    print(count)
+
+##for loop with specified steps to skip
+#for count in range(1,10,2):
+#    print(count);
+
+##specifying what values to use in the loop (For each loops like in vb)
+#for count in [1,2,3,4,5]:
+#    print(count);
+
+##For each loop with string variable for steps
+#for steps in ['red','blue','green','black']:
+#    turtle.color(steps)
+#    turtle.forward(100)
+#    turtle.left(90)
+
+
+
+
+
+
+
+for color in ['red','blue','green','yellow']:
+   turtle.color(color)
+   turtle.forward(100)
+   turtle.left(90)
